5d9a1b947dfeb0b81bacb7215909339081eb4f6a
Or Pravda <orpravda@Ors-MacBook-Pro.local>
2024-11-26 23:46:47 +0200

    v 3

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,81 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		if (str1 == "" || str2 == ""){
+			return true;
+
+		}
+		
+		char ch1, ch2;
+        boolean boolAnagram = false;
+
+        for (int i = 0; i < str1.length(); i++) {
+            ch1 = str1.charAt(i);
+            if ( ch1 <= 64 && ch1 <= 47 && ch1 >= 58 && ch1 >= 32 ) {
+                continue;
+            }
+            for (int j = 0; j < str2.length(); j++) {
+                ch2 = str2.charAt(j);
+
+                if ( ch1 == ch2 + 32 || ch1 == ch2 - 32 || ch1 == ch2) {
+                    boolAnagram = true;
+                    break;
+                } else {
+                    boolAnagram = false;
+                }
+            }
+            if (!boolAnagram) break;
+        }
+        return boolAnagram;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String newStr = "";
+		char ch;
+		for (int i = 0; i < str.length(); i++) {
+			ch = str.charAt(i);
+			if (ch >= 65 && ch <= 90) {
+				ch += 32;
+				newStr = newStr + ch;
+
+			} else if (ch >= 97 && ch <= 122) {
+				newStr = newStr + ch;
+
+			} else if (ch == 32) {
+				newStr = newStr + ch;
+			}
+		}
+		return newStr;		
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		char ch1, ch2;
+		String newString = "", smallerString = "";
+		int randNum = 0;
+		while (str.length() != 0) {
+			smallerString = "";
+			randNum = (int)(Math.random() * str.length());
+			ch1 = str.charAt(randNum);
+			if (ch1 == 32) {
+				continue;
+			}
+			if (ch1 >= 65 && ch1 <= 90) {
+				ch1 += 32;
+			}
+			newString = newString + ch1;
+			for (int i = 0; i < str.length(); i++) {
+				ch2 = str.charAt(i);
+				if (ch1 != ch2) {
+					smallerString = smallerString + ch2;
+				}
+			}
+			str = smallerString;
+		}
+		return newString;	
 	}
 }
